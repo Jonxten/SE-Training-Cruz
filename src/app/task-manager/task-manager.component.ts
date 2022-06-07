075b52a6c8c4fd8d4337b0eff5e653a0ff2caf5c
Justine Cruz <justin@quickreach.co>
2022-06-07 16:55:18 +0800

    [MOD] Added Paginator and Steppers for Add Task

--- src/app/task-manager/task-manager.component.ts
+++ src/app/task-manager/task-manager.component.ts
@@ -1,11 +1,11 @@
-import { Component, Injectable, OnInit, NgModule } from '@angular/core';
+import { Component, Injectable, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { AddTaskDialogComponent } from './add-task-dialog/add-task-dialog.component';
 import { DeleteTaskDialogComponent } from './delete-task-dialog/delete-task-dialog.component';
 import { EditTaskDialogComponent } from './edit-task-dialog/edit-task-dialog.component';
-import {Subject} from 'rxjs';
+import { Subject } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface Tasks {
   taskName: string;
@@ -18,6 +18,46 @@ export interface Tasks {
 }
 
 const TASK_DATA: Tasks[] = [
+  {taskName: 'Learn Angular', 
+  taskDescription: '- Watch Angular Materials - Output basic print' , 
+  tags: 'Programming', 
+  status: 'Ongoing', 
+  dateCreated: new Date("06-01-2022").toDateString(),
+  dateModified: '', 
+  dateCompleted: ''},
+
+  {taskName: 'Bake Buns', 
+  taskDescription: '- (.25 ounce) package active - 2 cups flour', 
+  tags: 'Cooking', 
+  status: 'New', 
+  dateCreated: new Date("05-25-2022").toDateString(),
+  dateModified: '', 
+  dateCompleted: ''},
+
+  {taskName: 'Learn C++', 
+  taskDescription: '- Watch C++ Materials - Output basic print', 
+  tags: 'Programming', 
+  status: 'Completed', 
+  dateCreated: new Date("04-26-2020").toDateString(),
+  dateModified: '', 
+  dateCompleted: new Date("04-30-2020").toDateString()},
+
+  {taskName: 'BAI Training', 
+  taskDescription: '- Learn C# - Learn SQL - Output basic print', 
+  tags: 'Programming, Work', 
+  status: 'Ongoing', 
+  dateCreated: new Date("05-30-2022").toDateString(),
+  dateModified: new Date("05-31-2022").toDateString(), 
+  dateCompleted: ''},
+  
+  {taskName: 'Learn CSS', 
+  taskDescription: '- Watch CSS Materials - Output basic print', 
+  tags: 'Programming', 
+  status: 'Completed', 
+  dateCreated: new Date("07-12-2022").toDateString(),
+  dateModified: '', 
+  dateCompleted: new Date("07-19-2022").toDateString()},
+
   {taskName: 'Learn Angular', 
   taskDescription: '- Watch Angular Materials - Output basic print' , 
   tags: 'Programming', 
@@ -85,10 +125,31 @@ export class TaskManagerComponent implements OnInit {
     'action'
   ];
 
-  searchTask = '';
-  // dataSource = new MatTableDataSource<Tasks>(TASK_DATA);
-  dataSource = [...TASK_DATA];
+  totalPosts = 0;
+  postsPerPage = 5;
+  currentPage = 1;
+  pageSizeOptions = [5,10,15];
   
+  searchTask = '';
+
+  dataSource = new MatTableDataSource<Tasks>(TASK_DATA);
+  // dataSource = [...TASK_DATA];
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(){
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+  }
+
   addDialog() {
     this.dialog.open(AddTaskDialogComponent, {
       width: '45%'
@@ -108,17 +169,7 @@ export class TaskManagerComponent implements OnInit {
   }
   
 
-  ngOnInit(): void {
-    // this.dataSource = new MatTableDataSource<Task>(this.TASK_DATA);
-    // this.loading = true;
-    // this.studentService.getStudents(this.postsPerPage, this.currentPage);
-    // this.studentService.studentChanged.subscribe((studData) => {
-    // // this.ELEMENT_DATA = studData.students;
-    // this.totalPosts = studData.postCount;
-    // this.dataSource = new MatTableDataSource<InstructorExemptions>(this.ELEMENT_DATA);
-    // this.loading = false;
-    // });
-  }
+  
 
   
 
